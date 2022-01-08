b753d93f3ed6812fcbad6cd8acaad0c6671f26cf
tuanvu0801junda <tuanvutruonglacnhatsinh0801@gmail.com>
2022-01-09 00:05:01 +0700

    add zalo (1)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,35 +1,34 @@
 <?php
 use Illuminate\Support\Facades\DB;
-use App\Models\ViettelAcc;
 use App\Models\ZaloAcc;
 use Illuminate\Support\Facades\Hash;
 
 function insertRandom(){
-    $acc1 = new ViettelAcc();
-    $acc1->viettelPhone = '1234567890';
-    $acc1->viettelPassword = Hash::make('123456');
-    $acc1->viettelBalance = 1500000;
+    $acc1 = new ZaloAcc();
+    $acc1->zaloPhone = '1234567890';
+    $acc1->zaloPassword = Hash::make('123456');
+    $acc1->zaloBalance = 1500000;
     $acc1->save();
 
-    $acc1 = new ViettelAcc();
-    $acc1->viettelPhone = '1234567891';
-    $acc1->viettelPassword = Hash::make('123457');
-    $acc1->viettelBalance = 1500000;
+    $acc1 = new ZaloAcc();
+    $acc1->zaloPhone = '1234567891';
+    $acc1->zaloPassword = Hash::make('123457');
+    $acc1->zaloBalance = 1500000;
     $acc1->save();
 
-    $acc1 = new ViettelAcc();
-    $acc1->viettelPhone = '1234567892';
-    $acc1->viettelPassword = Hash::make('123458');
-    $acc1->viettelBalance = 1500000;
+    $acc1 = new ZaloAcc();
+    $acc1->zaloPhone = '1234567892';
+    $acc1->zaloPassword = Hash::make('123458');
+    $acc1->zaloBalance = 1500000;
     $acc1->save();
 
-    $acc1 = new ViettelAcc();
-    $acc1->viettelPhone = '1234567893';
-    $acc1->viettelPassword = Hash::make('123459');
-    $acc1->viettelBalance = 1500000;
+    $acc1 = new ZaloAcc();
+    $acc1->zaloPhone = '1234567893';
+    $acc1->zaloPassword = Hash::make('123459');
+    $acc1->zaloBalance = 1500000;
     $acc1->save();
 
-    echo "<h2>Add db successfully!</h2>";
+    echo "<h2>Add db zalo successfully!</h2>";
 }
 
 insertRandom();
