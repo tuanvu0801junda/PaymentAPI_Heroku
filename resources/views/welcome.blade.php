886e48f58da522a60605d140b02319e8df28b536
tuanvu0801junda <tuanvutruonglacnhatsinh0801@gmail.com>
2022-01-08 23:59:22 +0700

    add viettel(1)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,41 +1,32 @@
 <?php
 use Illuminate\Support\Facades\DB;
-use App\Models\BankAcc;
 use App\Models\ViettelAcc;
 use App\Models\ZaloAcc;
 use Illuminate\Support\Facades\Hash;
 
 function insertRandom(){
-    $acc1 = new BankAcc();
-    $acc1->bankCard = '123456';
-    $acc1->bankName = 'Tuan Vu';
-    $acc1->bankCvv = Hash::make('1234');
-    $acc1->bankExpired = '10/26';
-    $acc1->bankBalance = 1500000;
+    $acc1 = new ViettelAcc();
+    $acc1->viettelPhone = '1234567890';
+    $acc1->viettelPassword = Hash::make('123456');
+    $acc1->viettelBalance = 1500000;
     $acc1->save();
 
-    $acc1 = new BankAcc();
-    $acc1->bankCard = '123457';
-    $acc1->bankName = 'Long Hoang';
-    $acc1->bankCvv = Hash::make('2345');
-    $acc1->bankExpired = '10/27';
-    $acc1->bankBalance = 1500000;
+    $acc1 = new ViettelAcc();
+    $acc1->viettelPhone = '1234567891';
+    $acc1->viettelPassword = Hash::make('123457');
+    $acc1->viettelBalance = 1500000;
     $acc1->save();
 
-    $acc1 = new BankAcc();
-    $acc1->bankCard = '123458';
-    $acc1->bankName = 'Ngoc Tran';
-    $acc1->bankCvv = Hash::make('3456');
-    $acc1->bankExpired = '10/28';
-    $acc1->bankBalance = 1500000;
+    $acc1 = new ViettelAcc();
+    $acc1->viettelPhone = '1234567892';
+    $acc1->viettelPassword = Hash::make('123458');
+    $acc1->viettelBalance = 1500000;
     $acc1->save();
 
-    $acc1 = new BankAcc();
-    $acc1->bankCard = '123459';
-    $acc1->bankName = 'Hieu Tran';
-    $acc1->bankCvv = Hash::make('5678');
-    $acc1->bankExpired = '10/29';
-    $acc1->bankBalance = 1500000;
+    $acc1 = new ViettelAcc();
+    $acc1->viettelPhone = '1234567893';
+    $acc1->viettelPassword = Hash::make('123459');
+    $acc1->viettelBalance = 1500000;
     $acc1->save();
 
     echo "<h2>Add db successfully!</h2>";
