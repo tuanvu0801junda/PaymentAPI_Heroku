eeb9cc73c4c56277e6e64c20b20ffad0a6227d74
tuanvu0801junda <tuanvutruonglacnhatsinh0801@gmail.com>
2022-01-09 15:19:05 +0700

    refactor mockdata for integration(2)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,21 +9,21 @@ function insertData(){
     $acc1 = new ZaloAcc();
     $acc1->zaloPhone = '0123789456';
     $acc1->zaloPassword = Hash::make('abcxyz');
-    $acc1->zaloBalance = 1500000;
+    $acc1->zaloBalance = 1600000;
     $acc1->save();
 
     $acc1 = new ViettelAcc();
     $acc1->viettelPhone = '0123789456';
     $acc1->viettelPassword = Hash::make('abcxyz');
-    $acc1->viettelBalance = 1500000;
+    $acc1->viettelBalance = 1600000;
     $acc1->save();
 
     $acc1 = new BankAcc();
-    $acc1->bankCard = '123459';
+    $acc1->bankCard = '123567';
     $acc1->bankName = 'HuongVi_HoangMinh';
     $acc1->bankCvv = Hash::make('4590');
     $acc1->bankExpired = '10/27';
-    $acc1->bankBalance = 1500000;
+    $acc1->bankBalance = 1600000;
     $acc1->save();
 }
 
