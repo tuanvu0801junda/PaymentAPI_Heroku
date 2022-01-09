2ec15821a2ccc117c7128b5f25d79cb6d4fd918d
tuanvu0801junda <tuanvutruonglacnhatsinh0801@gmail.com>
2022-01-09 15:29:24 +0700

    comment insert in view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,27 @@ use App\Models\BankAcc;
 use App\Models\ViettelAcc;
 use Illuminate\Support\Facades\Hash;
 
-function insertData(){
-    $acc1 = new BankAcc();
-    $acc1->bankCard = '123567';
-    $acc1->bankName = 'HuongVi_HoangMinh';
-    $acc1->bankCvv = Hash::make('4590');
-    $acc1->bankExpired = '10/27';
-    $acc1->bankBalance = 1600000;
-    $acc1->save();
-}
+// function insertData(){
+//     $acc1 = new ZaloAcc();
+//     $acc1->zaloPhone = '0123789456';
+//     $acc1->zaloPassword = Hash::make('abcxyz');
+//     $acc1->zaloBalance = 1600000;
+//     $acc1->save();
 
-insertData();
+//     $acc1 = new ViettelAcc();
+//     $acc1->viettelPhone = '0123789456';
+//     $acc1->viettelPassword = Hash::make('abcxyz');
+//     $acc1->viettelBalance = 1600000;
+//     $acc1->save();
+
+//     $acc1 = new BankAcc();
+//     $acc1->bankCard = '123567';
+//     $acc1->bankName = 'HuongVi_HoangMinh';
+//     $acc1->bankCvv = Hash::make('4590');
+//     $acc1->bankExpired = '10/27';
+//     $acc1->bankBalance = 1600000;
+//     $acc1->save();
+// }
+
+// insertData();
 ?>
